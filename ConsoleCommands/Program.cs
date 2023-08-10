271a7494b54d0546b9275eb65a8a60bd223b651e
d3h1 <denicabaravdic@gmail.com>
2023-08-10 12:53:28 -0700

    figured out git with Csharp

--- ConsoleCommands/Program.cs
+++ ConsoleCommands/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleCommands
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Some basic console commands 
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Hello There!");
+        }
+    }
+}
--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Variables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool canIVote = true;
+
+            Console.WriteLine("Biggest Integer: {0}", int.MaxValue);
+            Console.WriteLine("Smallest Integer: {0}", int.MinValue);
+
+            Console.WriteLine("Biggest Long: {0}", long.MaxValue);
+            Console.WriteLine("Smallest Long: {0}", long.MinValue);
+
+            decimal decPiVal = 3.1415926535897932384626433832M;
+            decimal decBigValue = 3.0000000000000000000000000000011M;
+
+            // {0} is a placeholder for these numbers 
+            Console.WriteLine("DEC : PI + bigNum = {0}", decPiVal + decBigValue);
+
+        }
+    }
+}
