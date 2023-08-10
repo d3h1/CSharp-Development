c2a788f9b6109df804b4fb6b379fce350e38b2af
d3h1 <denicabaravdic@gmail.com>
2023-08-10 12:05:04 -0700

    did console commands

---
+++ TestingCSharp/ConsoleCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestingCSharp
+{
+    public class ConsoleCommands
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
