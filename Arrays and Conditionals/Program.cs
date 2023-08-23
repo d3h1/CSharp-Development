25913f6918f87655d50859c9175d25bb6f84aa24
d3h1 <denicabaravdic@gmail.com>
2023-08-23 14:31:47 -0700

    created new folder for it

---
+++ Arrays and Conditionals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArraysAndConditonals
+{
+    public class Program
+    {
+        static void PrintArrays(int[] intArray, string mesage)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------MULT")
+        }
+    }
+}
