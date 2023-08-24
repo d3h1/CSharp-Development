1999be418313a9885d3130a5c255c5063f69fc75
d3h1 <denicabaravdic@gmail.com>
2023-08-24 15:27:53 -0700

    if else functionality

--- Conditonals/Program.cs
+++ Conditonals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Conditonals
 {
@@ -7,13 +8,38 @@ namespace Conditonals
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n\n\n");
-            Console.WriteLine("---------CONDITONALS-----------\n");
+            Console.WriteLine("---------CONDITONALS-----------");
+
+            // Relational Operators: > < >= <= == !=
+            // Logical Operators: && || !
+            int age = 17;
+            if ((age >= 5) && (age <= 7))
+            {
+                Console.WriteLine("Go to elementary school!");
+            }
+            else if ((age > 7) && (age < 13)) 
+            {
+                Console.WriteLine("Go to middle school!");
+            }
+            else if ((age > 14) && (age < 19)) 
+            {
+                Console.WriteLine("Go to high school!");
+            } 
+            else
+            {
+                Console.WriteLine("Go to college!");
+            }
+
+            if ((age < 14) || (age > 67)) {
+                Console.WriteLine("Not allowed to work!");
+            }
+
 
             // Set random base Object type array
             object[] randArr = { 1, 2, 3, "Bob", "Sue", 1.2345, true };
 
             // FOR LOOPS
-            Console.WriteLine("----------FOR LOOPS------------");
+            Console.WriteLine("\n\n----------FOR LOOPS------------");
             for (int i = 0; i < randArr.Length; i++)
             {
                 Console.WriteLine("Index : {0} -> Value : {1}", i, randArr[i]);
