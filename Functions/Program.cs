d74d7ed220330d4968ee0a002b32ca2b24768cae
denicabaravdic <denicabaravdic@gmail.com>
2023-08-25 15:23:45 -0700

    functions coming together

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -5,12 +5,29 @@ namespace Functions
     public class Program
     {
         // -------- FUNCTIONS ------------
+        // <Access Specifier> <Return Type> <Method Name>(Parameters)
+        // { <Body> }
+
+        // Access specifier determines wheter the function can be called from another class
+        /* 
+        PUBLIC : Can be accessed from anther class
+        PRIVATE : Cannot be accessed from another class
+        PROTECTED : Cannot be accessed by class but can be accessed from derived class 
+        */
+
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+        }
 
 
         // -------- END OF FUNCTIONS -----------
         static void Main(string[] args)
         {
-
+            SayHello();
         }
     }
 }
