fb679451c0a884273acdb440b127edc99fb5b64e
denicabaravdic <denicabaravdic@gmail.com>
2023-09-16 15:11:01 -0700

    learning about classes

---
+++ Classes/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    // We will use this to model real world objects
+    class Animal
+    {
+        // FIELDS
+        public string name;
+        public string sound;
+        
+        // STATIC FIELD
+        static int numOfAnimals = 0; // Static belongs to the class
+
+        // CONSTRUCTOR -- Allows for default values
+        public Animal()
+        {
+            
+            name = "No Name";
+            sound = "No Sound";
+            // Track number of animals ever made
+            numOfAnimals++;
+        }
+
+        // If name is provided but no sound
+        public Animal(string name = "No Name")
+        {
+            // THIS -- Refers to the objects name
+            this.name = name;
+            this.sound = "No Sound";
+            numOfAnimals++;
+        }
+
+
+
+    }
+}
