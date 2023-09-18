8f8940b6c92faf4d5acca94d56dfd035f7d71e23
d3h1 <denicabaravdic@gmail.com>
2023-09-18 14:46:48 -0700

    Doing more intermediate work on classes

---
+++ Classes Intermediate/Animals.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    // We will use this to model real world objects
+    class Animal
+    {
+        // FIELDS
+        public string name;
+        public string sound;
+
+        // STATIC FIELD
+        static int numOfAnimals = 0; // Static belongs to the class
+
+        // CONSTRUCTOR -- Allows for default values
+        public Animal()
+        {
+
+            name = "No Name";
+            sound = "No Sound";
+            // Track number of animals ever made
+            numOfAnimals++;
+        }
+
+        // THIS -- Refers to the objex
+        // If name is provided but no sound
+        public Animal(string name = "No Name")
+        {
+            this.name = name;
+            this.sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        // Setting up default values in the params
+        public Animal(string name = "No name", string sound = "No sound")
+        {
+            // This will assign the default or the value passed through
+            this.name = name;
+            this.sound = sound;
+            numOfAnimals++;
+        }
+
+        // Capabilities of class can come from Methods
+        // METHODS -- Special functions inside of classes
+        public void MakeSounds()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+        // Get total amount of animals created
+        // Static because numOfAnimals is static 
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
+
+
+    }
+}
---
+++ Classes Intermediate/ShapeMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class ShapeMath
+    {
+        // Static means you do not need to create an object to use it
+        public static double GetArea(
+            string shape = "",
+            double length1 = 0,
+            double length2 = 0
+            )
+        {
+            if (String.Equals("Rectangle", shape,
+                StringComparison.OrdinalIgnoreCase))
+            {
+                return length1 * length2;
+            }
+            else if (String.Equals("Triangle", shape,
+                StringComparison.OrdinalIgnoreCase))
+            {
+                return length1 * length2 / 2;
+            }
+            else if (String.Equals("Circle", shape,
+                StringComparison.OrdinalIgnoreCase))
+            {
+                return 3.14159 * Math.Pow(length1, 2);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
