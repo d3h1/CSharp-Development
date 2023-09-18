69d723968dd711ea91854d3b5311e27dfe73f406
d3h1 <denicabaravdic@gmail.com>
2023-09-18 16:12:10 -0700

    Getters and Setters done

--- Classes Intermediate/Animals.cs
+++ Classes Intermediate/Animals.cs
@@ -6,56 +6,99 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    // We will use this to model real world objects
+    // THIS IS NOT THE BEST WAY TO DO GETTERS AND SETTERS 
     class Animal
     {
         // FIELDS
-        public string name;
-        public string sound;
+        // PRIVATE to only be accessed through their getters and setters 
+        // If marked as private, not accessible from subclasses. 
+        private string name;
+        private string sound;
 
         // STATIC FIELD
-        static int numOfAnimals = 0; // Static belongs to the class
+        public static int numOfAnimals = 0; // Static belongs to the class
+        // CONSTANTS
+        public const string SHELTER = "Deni's Home For Animals";
+        // Define an ID number for each animal
+        //READONLY -- Can be set and cannot be changed
+        public readonly int idNum;
 
-        // CONSTRUCTOR -- Allows for default values
-        public Animal()
+        // DEFAULT CONSTRUCTOR if nothing is set
+        public Animal() : this("No Name", "No Sound") { }
+
+        // Constructor called only if a name only is passed 
+        public Animal(string name) : this(name, "No Sound") { }
+
+        // Constructor to take away all error messages in program with 'numOfAnimals' is not null here
+        public Animal(string name, string sound)
         {
+            SetName(name);
+            Sound = sound;
 
-            name = "No Name";
-            sound = "No Sound";
-            // Track number of animals ever made
-            numOfAnimals++;
+            NumOfAnimals = 1;
+
+            Random rnd = new Random();
+            idNum = rnd.Next(1, 214748249);
         }
-
-        // THIS -- Refers to the objex
-        // If name is provided but no sound
-        public Animal(string name = "No Name")
-        {
-            this.name = name;
-            this.sound = "No Sound";
-            numOfAnimals++;
-        }
-
-        // Setting up default values in the params
-        public Animal(string name = "No name", string sound = "No sound")
-        {
-            // This will assign the default or the value passed through
-            this.name = name;
-            this.sound = sound;
-            numOfAnimals++;
-        }
-
-        // Capabilities of class can come from Methods
-        // METHODS -- Special functions inside of classes
+     
         public void MakeSounds()
         {
             Console.WriteLine("{0} says {1}", name, sound);
         }
 
-        // Get total amount of animals created
-        // Static because numOfAnimals is static 
         public static int GetNumAnimals()
         {
             return numOfAnimals;
+        }
+
+        // SETTERS or MUTATORS -- Protect attributes.
+        // If bad name pass, we can go ahead and erject that
+        // THIS IS NOT THE BEST WAY TO DO GETTERS AND SETTERS -- You should do it all in like Sound
+        public void SetName(String name)
+        {
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name cannot contain numbers!");
+            }
+        }
+
+        // GETTERS or ACCESSORS - PRovide custom output
+        public string GetName()
+        {
+            return name;
+        }
+
+        // THIS IS THE BEST WAY TO SET UP SETTERS AND GETTERS 
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    sound = "No Sound!";
+                    Console.WriteLine("Sound is too long!");
+                }
+                else
+                {
+                    sound = value;
+                }
+            }
+        }
+
+        // GETTERS or SETTERS generated for us
+        public string Owner { get; set; } = "No Owner";
+
+        // Store more than one number of animals
+        public static int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
         }
 
 
--- Classes Intermediate/Program.cs
+++ Classes Intermediate/Program.cs
@@ -6,59 +6,21 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            // This is the struct version
-            Rectangle rect1;
-            rect1.length = 200;
-            rect1.width = 50;
-            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
-            // Constructor to create rectangle
-            Rectangle rect2 = new Rectangle(100, 40);
-            rect2 = rect1;
-            rect1.length = 33;
-            Console.WriteLine("Rect2.length: {0}", rect2.length);
-
-            Animal fox = new Animal()
-            {
-                name = "Red",
-                sound = "Raaw"
-            };
-
-            Console.WriteLine("# of Animals {0}", Animal.GetNumAnimals());
-
-            // Creating a static utility class
-            Console.WriteLine("Area of Rectangle: {0}", ShapeMath.GetArea("Rectangle", 5, 6));
-
-
-            // Data types by default cannot be null
-            // NULLABLE TYPES -- really needed for things like databases, 
-            int? randNum = null;
-            if (randNum == null)
-            {
-                Console.WriteLine("RandNum is null!");
-            }
-            if (!randNum.HasValue)
-            {
-                Console.WriteLine("RandNum is null!");
-            }
-
-        }
-        // STRUCT -- User Defined Type
-        // Much like classes, they have fields and methods
-        struct Rectangle
-        {
-            public double length;
-            public double width;
-
-            // Constructor method
-            public Rectangle(double l = 1, double w = 1) { length = l; width = w; }
-
-            // Calculate Area
-            public double Area()
-            {
-                return length * width;
-            }
+            Animal cat = new Animal();
+            // Cannot set it directly as we have to use setters now
+            cat.SetName("Mike");
+            // This was the best way to set up setters and getters
+            cat.Sound = "Meow";
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+            cat.Owner = "Deni";
+            Console.WriteLine("{0}'s owner is {1}", cat.GetName(), cat.Owner);
+            
+            Console.WriteLine("{0}'s shelter ID is {1}", cat.GetName(), cat.idNum);
+            // Since we are getting a static prop, we need to call the class first
+            Console.WriteLine("# of animals : {0}", Animal.NumOfAnimals);
 
 
         }
+        
     }
 }
