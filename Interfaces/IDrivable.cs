b8a25da7a39f0e084700205a0d6d9bcc9c553e8a
d3h1 <denicabaravdic@gmail.com>
2023-09-19 13:53:28 -0700

    Learning about interfaces

---
+++ Interfaces/IDrivable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    // INTERFACE -- Contract : Means that if you inherit from interface, youi must implement certain methods to find inside the interface 
+
+    // Nothing in an interface but ABSTRACT METHODS 
+
+    // INTERFACE commonly have names that are ADJECTIVES and start with capital I
+    interface IDrivable
+    {
+        int Wheels { get; set; }
+        double Speed { get; set; }
+
+        // Differnet abstract methods
+        void Move();
+        void Stop();
+    }
+}
---
+++ Interfaces/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Vehicle : IDrivable
+    {
+        // Creating a real world object
+        public string Brand { get; set; }
+
+        public Vehicle( string brand = "No Brand", 
+                        int wheels = 0,
+                        double speed = 0)
+        {
+            Brand = brand;
+            Wheels = wheels;
+            Speed = speed;
+        }
+
+        public double Speed { get; set; }
+        public int Wheels { get; set; }
+
+        public void Move()
+        {
+            Console.WriteLine($"The {Brand} moves forward at {Speed} MPH!");
+        } 
+        public void Stop()
+        {
+            Console.WriteLine($"The {Brand} stops!");
+            Speed = 0;
+        }
+    }
+}
