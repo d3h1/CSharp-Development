66f4bf4874bb7b02bcdffbe7dcda3f036df16b22
d3h1 <denicabaravdic@gmail.com>
2023-09-29 16:03:57 -0700

    making a warrior class

---
+++ Warriors/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warriors
+{
+    class Warrior
+    {
+        // Warrior is going to have diff attributes
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double AttackMax { get; set; }
+        public double BlockMax { get; set; }
+
+        // Random number generations
+        Random rnd = new Random();
+
+    }
+}
