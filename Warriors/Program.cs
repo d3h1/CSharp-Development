e3f4017c76b5508a83c6d329ae83bd2a19926c06
d3h1 <denicabaravdic@gmail.com>
2023-09-29 15:51:57 -0700

    starting a warrior battle

---
+++ Warriors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Warriors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // thor = new Warrior("Thor", 100, 26, 10)
+            // hulk = new Warrior("Hulk", 100, 26, 10)
+            // Battle.StartFight(thor, hulk)
+
+        }
+    }
+}
