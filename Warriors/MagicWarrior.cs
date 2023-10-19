577279dafb393aefcfdfed0ca3415a8dc6d8d45f
denicabaravdic <denicabaravdic@gmail.com>
2023-10-19 12:02:29 -0700

    warriors can damage each other and one can even teleport

---
+++ Warriors/MagicWarrior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warriors
+{
+    class MagicWarrior : Warrior
+    {
+        int teleportChance = 0;
+        CanTeleport teleportType = new CanTeleport();
+        
+        // Constructor creation
+        public MagicWarrior(string name = "Warrior", double health = 0, double attackMax = 0, double blockMax = 0, int teleportChance = 0) : base(name, health, attackMax, blockMax)
+        {
+            this.teleportChance = teleportChance;
+        }
+
+        // Override the block function as with a good teleport, they will take no damage
+        public override double Block()
+        {
+            Random rnd = new Random();
+            int rndDodge = rnd.Next(1, 100);
+
+            if (rndDodge < this.teleportChance)
+            {
+                Console.WriteLine($"{Name} {teleportType.teleport()}");
+                return 10000;
+            }
+            else
+            {
+                return base.Block();
+            }
+        }
+    }
+}
--- Warriors/Program.cs
+++ Warriors/Program.cs
@@ -6,8 +6,10 @@ namespace Warriors
     {
         static void Main(string[] args)
         {
-            Warrior thor = new Warrior("Thor", 100, 26, 10);
-            Warrior loki = new Warrior("Loki", 100, 26, 10);
+            Warrior thor = new Warrior("Warrior 1", 100, 26, 10);
+            //Warrior loki = new Warrior("Loki", 100, 26, 10);
+            MagicWarrior loki = new MagicWarrior("Warrior 2", 75, 20, 10, 50);
+
 
             Battle.StartFight(thor, loki);
 
