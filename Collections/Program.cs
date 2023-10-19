0c332c16f1b524bcbd87e4d2a98a42ff874af5a8
denicabaravdic <denicabaravdic@gmail.com>
2023-10-19 13:53:58 -0700

    some structure changes

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections; // Arrays
+using System.Collections.Generic; // Dictionaries
+
+namespace Collections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Region allows us to collapse parts of code
+            #region ArrayList Code 
+
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add("Bob");
+            arrayList.Add(1);
+
+            Console.WriteLine("Count: {0}", arrayList.Count);
+
+            #endregion
+        }
+    }
+}
